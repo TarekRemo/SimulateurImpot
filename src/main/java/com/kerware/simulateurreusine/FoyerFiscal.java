d377bafc38629a1372b7b466a7440ed642222e6a
courtoi223 <courtoi223@unicaen.fr>
2025-05-04 22:41:42 +0200

    corection of 10 first warning

--- src/main/java/com/kerware/simulateurreusine/FoyerFiscal.java
+++ src/main/java/com/kerware/simulateurreusine/FoyerFiscal.java
@@ -5,7 +5,7 @@ import com.kerware.SituationFamiliale;
 /**
  * Permet de représenter un foyer fiscal.
  */
-public class FoyerFiscal {
+public final class FoyerFiscal {
 
     private int revenuNetDeclarant1;
     private int revenuNetDeclarant2;
@@ -13,9 +13,9 @@ public class FoyerFiscal {
     private int nombreEnfants;
     private int nombreEnfantsHandicapes;
     private boolean parentIsole;
-    
+
     /**
-     * Constructeur permettant d'initialiser le foyer avec des valeurs par défauts
+     * Constructeur permettant d'initialiser le foyer avec des valeurs par défauts.
      */
     public FoyerFiscal() {
         this.revenuNetDeclarant1 = 0;
@@ -26,53 +26,109 @@ public class FoyerFiscal {
         this.parentIsole = false;
     }
 
-
+    /**
+     * @return le revenu net du déclarant 1
+     */
     public int getRevenuNetDeclarant1() {
         return revenuNetDeclarant1;
     }
 
+    /**
+     * Définit le revenu net du déclarant 1.
+     * @param revenu le revenu à définir
+     */
+    public void setRevenuNetDeclarant1(int revenu) {
+        this.revenuNetDeclarant1 = revenu;
+    }
+
+    /**
+     * @return le revenu net du déclarant 2
+     */
     public int getRevenuNetDeclarant2() {
         return revenuNetDeclarant2;
     }
 
+    /**
+     * Définit le revenu net du déclarant 2.
+     * @param revenu le revenu à définir
+     */
+    public void setRevenuNetDeclarant2(int revenu) {
+        this.revenuNetDeclarant2 = revenu;
+    }
+
+    /**
+     * @return la situation familiale
+     */
     public SituationFamiliale getSituationFamiliale() {
         return situationFamiliale;
     }
 
+    /**
+     * Définit la situation familiale du foyer.
+     * @param situation la situation familiale à définir
+     */
+    public void setSituationFamiliale(SituationFamiliale situation) {
+        this.situationFamiliale = situation;
+    }
+
+    /**
+     * @return le nombre d'enfants à charge
+     */
     public int getNombreEnfants() {
         return nombreEnfants;
     }
 
+    /**
+     * Définit le nombre d'enfants à charge.
+     * @param nbEnfants le nombre d'enfants
+     */
+    public void setNombreEnfants(int nbEnfants) {
+        this.nombreEnfants = nbEnfants;
+    }
+
+    /**
+     * @return le nombre d'enfants handicapés
+     */
     public int getNombreEnfantsHandicapes() {
         return nombreEnfantsHandicapes;
     }
 
+    /**
+     * Définit le nombre d'enfants handicapés à charge.
+     * @param nb le nombre d'enfants handicapés
+     */
+    public void setNombreEnfantsHandicapes(int nb) {
+        this.nombreEnfantsHandicapes = nb;
+    }
+
+    /**
+     * @return true si le parent est isolé, false sinon
+     */
     public boolean isParentIsole() {
         return parentIsole;
     }
-    
-    public void setRevenuNetDeclarant1(int revenuNetDecl1) {
-		this.revenuNetDeclarant1 = revenuNetDecl1;
-	}
 
-	public void setRevenuNetDeclarant2(int revenuNetDeclarant2) {
-		this.revenuNetDeclarant2 = revenuNetDeclarant2;
-	}
+    /**
+     * Définit si le foyer est celui d’un parent isolé.
+     * @param isole true si le parent est isolé
+     */
+    public void setParentIsole(boolean isole) {
+        this.parentIsole = isole;
+    }
 
-	public void setSituationFamiliale(SituationFamiliale situationFamiliale) {
-		this.situationFamiliale = situationFamiliale;
-	}
-
-	public void setNombreEnfants(int nbEnfants) {
-		this.nombreEnfants = nbEnfants;
-	}
-
-	public void setNombreEnfantsHandicapes(int nbEnfantsHandicapes) {
-		this.nombreEnfantsHandicapes = nbEnfantsHandicapes;
-	}
-
-	public void setParentIsole(boolean parentIsole) {
-		this.parentIsole = parentIsole;
-	}
+    /**
+     * Représentation textuelle du foyer fiscal.
+     * @return une chaîne représentant les attributs du foyer
+     */
+    @Override
+    public String toString() {
+        return "FoyerFiscal{" +
+                "revenuNetDeclarant1=" + revenuNetDeclarant1 +
+                ", revenuNetDeclarant2=" + revenuNetDeclarant2 +
+                ", situationFamiliale=" + situationFamiliale +
+                ", nombreEnfants=" + nombreEnfants +
+                ", nombreEnfantsHandicapes=" + nombreEnfantsHandicapes +
+                ", parentIsole=" + parentIsole +
+                '}';
+    }
 }
-
